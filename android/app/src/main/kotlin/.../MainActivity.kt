ccdfb66c3484e080605e21c68ff317f1cb262e08
Subhasish Lakshman <59129320+subhasishlak123@users.noreply.github.com>
2026-04-03 07:58:32 +0530

    Add MainActivity for Flutter app integration

---
+++ android/app/src/main/kotlin/.../MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.radio_app 
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // Leave this empty. V2 embedding handles plugin registration automatically.
+}
