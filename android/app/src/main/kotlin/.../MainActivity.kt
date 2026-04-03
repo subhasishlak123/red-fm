3f158cc3c8aa9a7c12f793cf69a3f6aa9dbcff92
Subhasish Lakshman <59129320+subhasishlak123@users.noreply.github.com>
2026-04-03 08:11:17 +0530

    Delete android/app/src/main/kotlin/... directory

--- android/app/src/main/kotlin/.../MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.radio_app 
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // Leave this empty. V2 embedding handles plugin registration automatically.
-}
